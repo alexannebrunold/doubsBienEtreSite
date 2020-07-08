11575bd7c577981cb655366d7f1777eefce391b6
Alexanne Brunold <alexanne.brunold@hetic.net>
2020-07-08 12:26:32 +0200

    components Header & Footer

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,31 @@
 <template>
 	<div id="app">
-		<div id="nav">
-			<router-link to="/">Home</router-link>
-			|
-			<router-link to="/about">About</router-link>
-		</div>
+		<Header />
+
 		<router-view />
+		<Footer />
 	</div>
 </template>
 
-<style lang="scss">
-#app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
+<script>
+import Header from "@/components/Header.vue"
+import Footer from "@/components/Footer.vue"
+
+export default {
+	name: "Home",
+	components: {
+		Header,
+		Footer,
+	},
 }
+</script>
+
+<style lang="scss">
+// #app {
+// 	font-family: "Josefin Sans", sans-serif;
+// 	-webkit-font-smoothing: antialiased;
+// 	-moz-osx-font-smoothing: grayscale;
+// 	text-align: center;
+// 	color: $secondaryPink;
+// }
 </style>
