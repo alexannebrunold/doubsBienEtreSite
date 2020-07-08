cc7dd7cd9e3b6e30b6dedce0cf37c409fb0c32ee
Alexanne Brunold <alexanne.brunold@hetic.net>
2020-07-08 15:49:43 +0200

    accueil landing

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,19 @@
 		<Header />
 
 		<router-view />
-		<Footer />
+		<!-- <Footer /> -->
 	</div>
 </template>
 
 <script>
 import Header from "@/components/Header.vue"
-import Footer from "@/components/Footer.vue"
+// import Footer from "@/components/Footer.vue"
 
 export default {
 	name: "Home",
 	components: {
 		Header,
-		Footer,
+		// Footer,
 	},
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,17 @@
 <template>
 	<div class="home">
-		<!-- <Button textButton="Lire la suite" /> -->
+		<span class="home__background">
+			<img src="../assets/img/background.jpg" alt="" />
+			<h1 class="home__background--citation">
+				« Certains regardent la vase au fond de l'étang, d'autres contemplent la fleur de lotus à la
+				surface de l'eau, il s'agit d'un choix. »
+			</h1>
+			<h2 class="home__background--auteur">Le Dalaï Lama</h2>
+		</span>
 	</div>
 </template>
 
-//
+<!-- <Button textButton="Lire la suite" /> -->
 <script>
 // import Button from "@/components/Button.vue"
 
@@ -15,3 +22,34 @@ export default {
 	},
 }
 </script>
+
+<style lang="scss" scoped>
+.home {
+	width: 100vw;
+
+	&__background {
+		font-family: "Dancing Script";
+		font-size: 20px;
+		font-weight: bold;
+		line-height: 1.4em;
+		text-align: center;
+		& img {
+			max-width: 100%;
+			height: 273px;
+			z-index: -2;
+			padding-right: 0;
+			padding-left: 0;
+		}
+		&--citation {
+			padding-right: 16px;
+			padding-left: 16px;
+			margin-top: -80%;
+		}
+		&--auteur {
+			margin-top: 4%;
+			color: $secondaryPink;
+			font-size: 22px;
+		}
+	}
+}
+</style>
